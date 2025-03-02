2733b9b959ec1d3b475b40e688ccc71ddf17fb1f
Mochamad Faishal Amir <mochamad.faishal@efishery.com>
2025-03-02 22:08:06 +0700

    fix: register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Form, Input, Button, Card } from "antd";
-import axios from "axios";
+import axiosInstance from "../utils/axios";
+import { useNavigate } from "react-router-dom";
 
 const Register: React.FC = () => {
   const [form] = Form.useForm();
+  const navigate = useNavigate();
 
   const onFinish = async (values: any) => {
     try {
-      await axios.post("/api/auth/register", values);
+      await axiosInstance.post("/auth/register", values);
       alert("Registration successful!");
+      navigate("/login");
     } catch (error) {
       console.error(error);
       alert("Registration failed. Please try again.");
@@ -19,11 +22,30 @@ const Register: React.FC = () => {
     <Card title="Register" style={{ maxWidth: 400, margin: "auto" }}>
       <Form form={form} onFinish={onFinish} layout="vertical">
         <Form.Item
-          label="Username"
-          name="username"
+          label="Email"
+          name="email"
           rules={[
-            { required: true, message: "Please input your username!" },
-            { min: 3, message: "Username must be at least 3 characters." },
+            {
+              required: true,
+              type: "email",
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Full Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+              type: "string",
+              min: 3,
+              max: 30,
+              message: "Name must be min 3 and max 30 characters!",
+            },
           ]}
         >
           <Input />
